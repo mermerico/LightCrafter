82a377f8972d35baa45b589b21564ced86a29dec
Mark Cafaro <mark.cafaro@gmail.com>
2014-03-19 15:29:49 -0700

    Updated LcrMonitor to only fallback on hardcoded values

--- LcrMonitor.m
+++ LcrMonitor.m
@@ -14,12 +14,16 @@ classdef LcrMonitor < Monitor
     methods (Access = protected)
         
         function r = getRefreshRate(obj)
+            r = getRefreshRate@Monitor(obj);
+            
             % HACK: We do not currently have a way to get precise non-integer monitor refresh rates. These rates are
             % reported by the LightCrafter 4500 EDID.
-            if obj.resolution == Lcr4500.NATIVE_RESOLUTION
-                r = 59.94;
-            else
-                r = 59.81;
+            if r == 59
+                if obj.resolution == Lcr4500.NATIVE_RESOLUTION
+                    r = 59.94;
+                else
+                    r = 59.81;
+                end
             end
         end
         
